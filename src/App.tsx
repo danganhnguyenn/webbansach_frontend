006f61f344d091af0dd139fd6f4fc25bd33ee243
danganhnguyenn <danganhnguyenn@gmail.com>
2025-07-23 18:19:02 +0700

    Thêm banner và carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import "./App.css";
-import Navbar from "./Navbar";
+
+import Footer from "./layouts/header-footer/Footer";
+
+import Navbar from "./layouts/header-footer/Navbar";
+import HomePage from "./layouts/homepage/HomePage";
+
+
 
 function App() {
    return (
-     <div>
-        <Navbar />
-    </div>
+   <>
+         <div><Navbar /></div>
+         <HomePage />
+         <div><Footer /></div>
+      
+   </>
    );
 }
 
